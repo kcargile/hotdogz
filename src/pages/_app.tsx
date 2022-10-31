95b43e0ff979f92dfb2e5a814654baf070bf852d
Kristopher Cargile <kris@kriscargile.com>
2022-10-31 15:48:37 -0600

    remove test apollo provider junk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
 import GlobalLayout from "@components/GlobalLayout";
 import { GlobalSettingsContextProvider } from "@context/GlobalSettingsContext";
 import "@fontsource/roboto/300.css";
@@ -10,27 +9,14 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-    const client = new ApolloClient({
-        cache: new InMemoryCache(),
-        link: createHttpLink({
-            headers: {
-                access_token: process.env.CONTENTSTACK_DELIVERY_TOKEN!
-            },
-            uri: `${process.env.CONTENTSTACK_BASE_URL}/${process.env.CONTENTSTACK_API_KEY}?environment=${process.env.CONTENTSTACK_ENVIRONMENT}`
-        }),
-        ssrMode: true
-    });
-
     return (
-        <ApolloProvider client={client}>
-            <GlobalSettingsContextProvider>
-                <Head>
-                    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
-                </Head>
-                <GlobalLayout>
-                    <Component {...pageProps} />
-                </GlobalLayout>
-            </GlobalSettingsContextProvider>
-        </ApolloProvider>
+        <GlobalSettingsContextProvider>
+            <Head>
+                <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
+            </Head>
+            <GlobalLayout>
+                <Component {...pageProps} />
+            </GlobalLayout>
+        </GlobalSettingsContextProvider>
     );
 }
