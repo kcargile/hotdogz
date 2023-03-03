de0d2245df9bf9f6ef32f31248a493d4ff1dc55e
Kristopher Cargile <kris@kriscargile.com>
2023-03-02 20:45:20 -0700

    extract cs client to type; housekeeping

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,18 @@ import HtmlHead from "@components/html/HtmlHead";
 import MainContent from "@components/layout/MainContent";
 import { GlobalSettings } from "@context/GlobalSettings";
 import { GlobalSettingsContext } from "@context/GlobalSettingsContext";
+import { ContentStackClient } from "@core/cs/ContentStackClient";
 import Container from "@mui/material/Container";
 import { default as Grid, default as Item } from "@mui/material/Unstable_Grid2";
 import { PAGE_FALLBACK_TITLE } from "@theme/Constants";
-import contentstack from "contentstack";
 import { GetServerSideProps } from "next";
 import React, { useContext, useEffect } from "react";
 
 // TODO: fix breakpoint issues in grid
+
+// TODO: 1. Fix global settings query
+// TODO: 2. Fix main content render settings query
+// TODO: 4. Prune manual GQL shit
 
 interface IndexProps {
     page: any;
@@ -85,13 +89,8 @@ export default function Index({ page, settings }: IndexProps) {
 // };
 
 export const getServerSideProps: GetServerSideProps<IndexProps> = async (context) => {
-    const stack = contentstack.Stack({
-        api_key: process.env.CONTENTSTACK_API_KEY!,
-        delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN!,
-        environment: process.env.CONTENTSTACK_ENVIRONMENT!
-    });
-
-    const query = stack.ContentType("page").Query();
+    const cs = new ContentStackClient().Client;
+    const query = cs.ContentType("page").Query();
     const result = await query
         .where("url", context.resolvedUrl)
         .includeReference(["featured_content", "main_content"])
@@ -99,9 +98,9 @@ export const getServerSideProps: GetServerSideProps<IndexProps> = async (context
         .toJSON()
         .find();
 
-    if (result.length !== 1 || result[0].length !== 1) {
+    if (result[0] && result[0].length !== 1) {
         // there should only be one homepage
-        throw new Error("Doggonnit! Some kat has jacked up this page. Sick em!!");
+        throw new Error("Doggonnit, some kat jacked up this page. Sick em!! 🐕");
     }
 
     const page = result[0][0];
