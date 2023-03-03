3c6994f72153c3292c12ccb4daf6101c350ef519
Kristopher Cargile <kris@kriscargile.com>
2023-03-03 13:01:50 -0700

    extract methods; version bump

--- src/lib/context/GlobalSettings.ts
+++ src/lib/context/GlobalSettings.ts
@@ -10,3 +10,26 @@ export type GlobalSettings = {
     siteTitle: string;
     siteLogoUrl: string;
 };
+
+export const fromContentType = (page: any, settings: any) => {
+    if (!settings) {
+        return undefined;
+    }
+
+    const headerNav = new Array<NavigationItem>();
+    if (page) {
+        page.header[0].primary_navigation[0].items.map((i: any) => {
+            headerNav.push({ title: i.target_external.title || "", href: i.target_external.href || "#" });
+        });
+    }
+
+    return {
+        attribution: settings.attribution,
+        copyright: settings.copyright,
+        fallbackMysteryImageUrl: settings.default_dog_image.url,
+        faviconUrl: settings.favicon.url,
+        headerNavItems: headerNav,
+        siteTitle: settings.site_title,
+        siteLogoUrl: settings.logo.url
+    } as GlobalSettings;
+};
